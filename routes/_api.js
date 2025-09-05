967d81abf74e26d2f3479588ad14b895974c47f3
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-05 18:51:33 +0600

    add moltepul user

--- routes/_api.js
+++ routes/_api.js
@@ -6,8 +6,8 @@ router.get('/', (req, res) => {
         {
             id: Math.random(),
             name: (function () {
-                const firstNames = ["Alice", "Bob", "Charlie", "Diana", "Ethan"];
-                const lastNames = ["Smith", "Jones", "Williams", "Brown", "Davis"];
+                const firstNames = ["ashraful","sojon","jamal","kamal","forhad","afaj","kmal","lalmiya","manya"];
+                const lastNames = ["pathan","houlader","chudory","khan","miya","islam","mirja","banerjy","hasan"];
 
                 function generateRandomFullName() {
                     const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
@@ -18,7 +18,67 @@ router.get('/', (req, res) => {
 
             }()),
             aeg: Math.floor((Math.random() * 10) + 12)
-        }
+        },{
+            id: Math.random(),
+            name: (function () {
+                const firstNames = ["ashraful","sojon","jamal","kamal","forhad","afaj","kmal","lalmiya","manya"];
+                const lastNames = ["pathan","houlader","chudory","khan","miya","islam","mirja","banerjy","hasan"];
+
+                function generateRandomFullName() {
+                    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+                    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+                    return `${randomFirstName} ${randomLastName}`;
+                }
+                return generateRandomFullName()
+
+            }()),
+            aeg: Math.floor((Math.random() * 10) + 12)
+        },{
+            id: Math.random(),
+            name: (function () {
+                const firstNames = ["ashraful","sojon","jamal","kamal","forhad","afaj","kmal","lalmiya","manya"];
+                const lastNames = ["pathan","houlader","chudory","khan","miya","islam","mirja","banerjy","hasan"];
+
+                function generateRandomFullName() {
+                    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+                    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+                    return `${randomFirstName} ${randomLastName}`;
+                }
+                return generateRandomFullName()
+
+            }()),
+            aeg: Math.floor((Math.random() * 10) + 12)
+        },{
+            id: Math.random(),
+            name: (function () {
+                const firstNames = ["ashraful","sojon","jamal","kamal","forhad","afaj","kmal","lalmiya","manya"];
+                const lastNames = ["pathan","houlader","chudory","khan","miya","islam","mirja","banerjy","hasan"];
+
+                function generateRandomFullName() {
+                    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+                    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+                    return `${randomFirstName} ${randomLastName}`;
+                }
+                return generateRandomFullName()
+
+            }()),
+            aeg: Math.floor((Math.random() * 10) + 12)
+        },{
+            id: Math.random(),
+            name: (function () {
+                const firstNames = ["ashraful","sojon","jamal","kamal","forhad","afaj","kmal","lalmiya","manya"];
+                const lastNames = ["pathan","houlader","chudory","khan","miya","islam","mirja","banerjy","hasan"];
+
+                function generateRandomFullName() {
+                    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+                    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+                    return `${randomFirstName} ${randomLastName}`;
+                }
+                return generateRandomFullName()
+
+            }()),
+            aeg: Math.floor((Math.random() * 10) + 12)
+        },
     ]
     res.send(data)
 })
