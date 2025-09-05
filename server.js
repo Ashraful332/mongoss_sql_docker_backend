c2eb0f56101d23e5f0ad6fadf48a647155a56f10
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-05 17:31:49 +0600

    add the routes and the statick code

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cors()); 
+
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send('Server is running, Express 1.0!');
 });
 
 app.listen(port, () => {
